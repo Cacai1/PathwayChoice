f19ec2c257b2270cc233dd6e67838cc64eb9a67d
Cacai1 <147081699+Cacai1@users.noreply.github.com>
2025-01-07 13:46:05 -0700

    Second Initial

---
+++ PathwayChoice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PathwayChoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
