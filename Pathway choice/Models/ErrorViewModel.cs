2029c4ec5d0bc9a8aa10588f50b34acdfd6fcfae
Cacai1 <147081699+Cacai1@users.noreply.github.com>
2025-01-07 13:40:06 -0700

    Deletion

--- Pathway choice/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pathway_choice.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
